750ce2d1cfa1b8e921768961116a9b939866d5c5
mrymhdd0@gmail.com <mrymhdd0@gmail.com>
2022-11-29 13:58:42 +0200

    update BL

--- BL/ExpensesBL.cs
+++ BL/ExpensesBL.cs
@@ -8,5 +8,46 @@ namespace BL
 {
     class ExpensesBL
     {
+        private IBudgetDAL _budgetDAL;
+        IMapper mapper;
+
+        public BudgetBL(IBudgetDAL budgetDAL)
+        {
+            _budgetDAL = budgetDAL;
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>();
+            });
+            mapper = config.CreateMapper();
+        }
+
+        public List<BudgetDTO> GetAllBudget()
+        {
+            List<Budget> budgetList = _budgetDAL.GetAllBudgets();
+            List<BudgetDTO> listBudgetDTO = mapper.Map<List<Budget>, List<BudgetDTO>>(budgetList);
+            return listBudgetDTO;
+        }
+
+
+        public bool AddBudget(BudgetDTO budgetDTO)
+        {
+            Budget currentBudget = mapper.Map<BudgetDTO, Budget>(budgetDTO);
+            bool isSucsess = _budgetDAL.AddBudget(currentBudget);
+            return isSucsess;
+        }
+
+        public bool UpdateBudget(BudgetDTO budgetDTO)
+        {
+            Budget currentBudget = mapper.Map<BudgetDTO, Budget>(budgetDTO);
+            bool isSucsess = _budgetDAL.UpdateBudget(currentBudget.Id, currentBudget);
+            return isSucsess;
+        }
+
+        public bool DeleteBudget(BudgetDTO budgetDTO)
+        {
+            int idToDelete = budgetDTO.Id;
+            bool isSucsess = _budgetDAL.DeleteBudget(idToDelete);
+            return isSucsess;
+        }
     }
 }
