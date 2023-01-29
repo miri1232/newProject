bdf744f6342fd8168aa88dfccebd3f56324023b7
mrymhdd0@gmail.com <mrymhdd0@gmail.com>
2023-01-29 21:46:33 +0200

    addIncome componnate

--- DAL/Models/dbBudgetContext.cs
+++ DAL/Models/dbBudgetContext.cs
@@ -43,9 +43,11 @@ namespace DAL.Models
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server= (LocalDB)\\MSSQLLocalDB;Database= dbBudget;Trusted_Connection=True;");
             }
+
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
