77aef88f365d0c3dc8c877292f7928341b227969
Miri <miri1232@gmail.com>
2023-02-06 22:58:41 +0200

    component Update Password

--- budget/src/app/Components/Users/update-password/update-password.component.ts
+++ budget/src/app/Components/Users/update-password/update-password.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/Classes/User';
+import { UserService } from 'src/app/Services/user.service';
+import { Logging } from 'src/shared/log.service';
 
 @Component({
   selector: 'app-update-password',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatePasswordComponent implements OnInit {
 
-  constructor() { }
+IsSend:boolean=false;
+  IdToLogin !: string;
+  PassToLogin !: string;
+  PassToUpdate !: string;
+  UserToUpdate = new User();
 
+  @Output() passToLogin: EventEmitter<number> = new EventEmitter()
+
+  constructor(
+    private log:Logging,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router:Router,
+    private route:ActivatedRoute
+  ) { }
+
+
+  
   ngOnInit(): void {
+    
   }
+
+
+SendPass(){
+this.userService.GetUserByID(this.IdToLogin).subscribe(res1 => {
+ if(res1!=undefined) alert(res1.email + "נשלחה סיסמא חדשה לכתובת המייל ");
+ this.IsSend=true;
+});
+}
+
+  UpdateUser() {
+    this.userService.LoginUserByID(this.IdToLogin, this.PassToLogin).subscribe(res => {
+      if (res!=undefined) {
+        this.userService.GetUserByID(this.IdToLogin).subscribe(res1 => {
+          this.UserToUpdate = res1;
+          this.UserToUpdate.password = this.PassToUpdate;
+   this.userService.UpdateUser(this.UserToUpdate).subscribe(res1 => {
+    console.log("curent user ======>", res1)
+        alert(this.UserToUpdate.firstName + " הסיסמא עודכנה בהצלחה ");
+        this.log.ActiveUser = this.UserToUpdate;
+        this.IsSend=false;
+        this.router.navigate(['/ListBudgets']);
+      });
+    });
+    }
+  }
+);
+
+}
 
 }
--- budget/src/app/Services/user.service.ts
+++ budget/src/app/Services/user.service.ts
@@ -28,38 +28,16 @@ export class UserService {
     return this.http.get<User>(this.V_API + '/GetUserByID',{responseType: 'json',params:p});
   }
 
-  // newUser:User=new User();
-  //  newUser.FirstName=FirstName;
-  //  newUser.LastName',LastName)
-  //  newUser.Id',userId)
-  //  newUser.Phone',Phone)
-  //  newUser.Email',Email)
-  //  newUser.DateBirth',DateBirth)
-  //  newUser.password',password);
-
+  
   AddUser(newUser:User):Observable<boolean> {
-        // const p =new HttpParams()
-        //  .set('FirstName',newUser.FirstName)
-        //  .set('LastName',newUser.LastName)
-        //  .set('Id',newUser.Id)
-        //  .set('Phone',newUser.Phone)
-        //  .set('Email',newUser.Email)
-        //  .set('DateBirth', newUser.DateBirth)
-        //  .set('password',newUser.Password);
-        // const p=new HttpParams().set('User',"newUser");
 
         return this.http.post<boolean>(this.V_API + '/AddUser',newUser);
   }
-  // AddUser(newUser: User): Observable<boolean> {
-  //   const u=new HttpParams().set('userDTO',newUser);
-    
-  //   return this.http.post<boolean>(this.V_API + '/AddUser',{params:u});
-  // }
+ 
   
-  // UpdateUser(newUser: User): Observable<boolean> {
-  //   const u=new HttpParams().set('userDTO',newUser);
+  UpdateUser(udateUser: User): Observable<boolean> {
     
-  //   return this.http.post<boolean>(this.V_API + '/UpdateUser',{params:u});
-  // }
+    return this.http.put<boolean>(this.V_API + '/UpdateUser',udateUser);
+  }
 
 }
